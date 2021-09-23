302a39f09d7cb28919cca8476f7c00388eed3969
daniel <yoojeenkim@gmail.com>
2021-09-23 19:54:42 -0400

    used all the given hints from the styling, implementing js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Work-Day-Planner
+
+https://momentjs.com/docs
+
+https://getbootstrap.com/docs/5.1/getting-started/introduction/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,52 @@
 var time = moment();
+var hourNine = $("#9");
+var hourTen = $("#10");
+var hourEleven = $("#11");
+var hourTwelve = $("#12");
+var hourThirteen = $("#13");
+var hourFourteen = $("#14");
+var hourFifteen = $("#15");
+var hourSixteen = $("#16");
+var hourSeventeen = $("#17");
+var saveBtn = $(".saveBtn");
 
 $("#currentDay").text(moment().format('MMMM Do, YYYY'));
+
+function startScheduler() {
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    })
+}
+
+saveBtn.on("click", function() {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time,schedule);
+});
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function() {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future");
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+startScheduler();
+pastPresentFuture();
